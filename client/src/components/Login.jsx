e1d600fee9e2ef7f5c38f9c8dcacea1c097f2d12
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-11-12 22:02:37 +0700

    change height to 80vh

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <div className="h-[900px] w-[1170px] rounded-lg my-16 mx-auto bg-[url('../../public/images/bg-login.png')] bg-no-repeat">
+      <div className="h-[80vh] w-[1170px] rounded-lg my-16 mx-auto bg-[url('../../public/images/bg-login.png')] bg-no-repeat">
         <div className="flex gap-4">
           <div className="flex-[1] flex justify-center">
-            <div className="h-[900px] flex items-center">
+            <div className="h-[80vh] flex items-center">
               <div className="bg-white p-8 h-max bg-opacity-75 z-50 rounded-lg w-[539px]">
                 <div className="w-full">
                   <div className="flex justify-center text-center items-center px-8">
@@ -27,7 +27,9 @@ const Login = () => {
                           type="text"
                           placeholder="username"
                         />
-                        <label className="mt-2" htmlFor="password">Password</label>
+                        <label className="mt-2" htmlFor="password">
+                          Password
+                        </label>
                         <input
                           className="px-4 rounded-lg"
                           type="password"
