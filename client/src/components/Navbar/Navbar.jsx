50f19364a52252135864c281040f45cb69d3f9e1
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-12-07 21:12:35 +0700

    updated vitejs, responsive navbar v1.1

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,6 +8,11 @@ import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
   const [name, setName] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
 
   axios.defaults.withCredentials = true;
   // useEffect(() => {
@@ -70,18 +75,26 @@ const Navbar = () => {
                 </Link>
               </>
             ) : (
-              <Link to={"/login"}>
-                <button
-                  type="button"
-                  class="text-white bg-blue-700 sm:hidden md:block lg:block xl:block 2xl:block hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center  lg:mr-0 dark:bg-blue-600"
-                >
-                  Login
-                </button>
-              </Link>
+              <>
+                <Link to={"/login"}>
+                  <button
+                    type="button"
+                    class="text-white bg-blue-700 sm:hidden md:block lg:block xl:block 2xl:block hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center lg:mr-0 dark:bg-blue-600"
+                  >
+                    Login
+                  </button>
+                </Link>
+                <div className="sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
+                  <span>
+                    <IoClose />
+                  </span>
+                </div>
+              </>
             )}
           </div>
           <div
-            class="items-center justify-between w-full md:flex md:w-auto md:order-1"
+            class="items-center justify-between w-full md:flex sm:hidden
+            lg:block xl:block 2xl:block md:w-auto md:order-1"
             id="navbar-sticky"
           >
             <ul class="flex flex-col md:p-0 md:space-x-8 xl:w-full xl:p-0 font-medium rounded-lg xl:flex-row xl:space-x-8 xl:mt-0 xl:border-0 md:flex md:flex-row sm:hidden lg:flex lg:flex-row">
@@ -113,12 +126,42 @@ const Navbar = () => {
                   </button>
                 </Link>
               </li>
-              <li>
-                <div className="sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
-                  <IoClose />
-                </div>
-              </li>
             </ul>
+
+            {/* {isOpen && (
+              <>
+                <ul class="flex flex-col md:p-0 md:space-x-8 xl:w-full xl:p-0 font-medium rounded-lg xl:flex-row xl:space-x-8 xl:mt-0 xl:border-0 md:flex md:flex-row sm:hidden lg:flex lg:flex-row">
+                  <li>
+                    <Link to={"/"}>
+                      <button class="block py-2 pl-3 pr-4 text-black hover:text-blue-700 focus:text-blue-700 rounded  xl:p-0">
+                        หน้าหลัก
+                      </button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/about"}>
+                      <button class="block py-2 pl-3 pr-4 text-gray-900 rounded focus:text-blue-700 hover:text-blue-700 xl:p-0">
+                        เกี่ยวกับ
+                      </button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/creator"}>
+                      <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-blue-700 xl:p-0 focus:text-blue-700">
+                        ผู้จัดทำ
+                      </button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/help"}>
+                      <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-blue-700 xl:p-0 focus:text-blue-700">
+                        ติดต่อ/แจ้งปัญหา
+                      </button>
+                    </Link>
+                  </li>
+                </ul>
+              </>
+            )} */}
           </div>
         </div>
       </nav>
