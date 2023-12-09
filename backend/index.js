a0ffd6f90aaa6f81e9c91ca24e38f56ecdf58ac4
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-12-09 18:32:52 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://sarp-a8dff5e6e541.herokuapp.com",
+      "https://sarp-a8dff5e6e541.herokuapp.com/api",
       "https://sarp.vercel.app",
     ],
     methods: ["POST", "GET"],
