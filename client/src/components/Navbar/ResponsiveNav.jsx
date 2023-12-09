554ee675b92e2c20a31e1af05f037aa25aa6a128
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-12-09 18:27:49 +0700

    commit

--- client/src/components/Navbar/ResponsiveNav.jsx
+++ client/src/components/Navbar/ResponsiveNav.jsx
@@ -45,7 +45,7 @@ const ResponsiveNav = ({ currentUser }) => {
               <Link to={"/login"}>
                 <button
                   type="button"
-                  className="text-white bg-blue-700 block md:block lg:block xl:block 2xl:block hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center mb-4 lg:mr-0 dark:bg-blue-600"
+                  className="text-white bg-blue-700 block md:block lg:block xl:block 2xl:block hover:bg-blue-800 font-medium rounded-lg text-sm px-4 ml-2 py-2 text-center mb-4 lg:mr-0 dark:bg-blue-600"
                 >
                   Login
                 </button>
