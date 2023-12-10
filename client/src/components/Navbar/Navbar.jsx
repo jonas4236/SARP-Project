56d3eb9a71630f16d11a8a3637a7c76151d174eb
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-12-10 16:49:52 +0700

    changed about text

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
                     type="button"
                     className="text-white bg-green-500 hover:bg-green-600 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0  dark:focus:ring-blue-800 ml-4"
                   >
-                    บันทึกนักเรียน
+                    บันทึกการเข้าเรียน
                   </button>
                 </Link>
               </>
