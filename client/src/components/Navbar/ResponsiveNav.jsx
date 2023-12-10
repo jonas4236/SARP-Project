d0a9d4876567bfbaa3659f37d8f1c4d27e069726
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-12-10 12:16:48 +0700

    added margin left

--- client/src/components/Navbar/ResponsiveNav.jsx
+++ client/src/components/Navbar/ResponsiveNav.jsx
@@ -12,28 +12,28 @@ const ResponsiveNav = ({ currentUser }) => {
         <ul className="flex flex-col md:p-0 md:space-x-8 xl:w-full xl:p-0 font-medium rounded-lg xl:flex-row xl:space-x-8 xl:mt-0 xl:border-0 md:flex md:flex-row sm:hidden lg:flex lg:flex-row">
           <li>
             <Link to={"/"}>
-              <button className="block py-2 pl-3 pr-4 text-black hover:text-blue-700 focus:text-blue-700 rounded  xl:p-0">
+              <button className="block py-2 pl-3 pr-4 ml-4 lg:ml-0 xl:ml-0 text-black hover:text-blue-700 focus:text-blue-700 rounded  xl:p-0">
                 หน้าหลัก
               </button>
             </Link>
           </li>
           <li>
             <Link to={"/about"}>
-              <button className="block py-2 pl-3 pr-4 text-gray-900 rounded focus:text-blue-700 hover:text-blue-700 xl:p-0">
+              <button className="block py-2 pl-3 pr-4 ml-4 lg:ml-0 xl:ml-0 text-gray-900 rounded focus:text-blue-700 hover:text-blue-700 xl:p-0">
                 เกี่ยวกับ
               </button>
             </Link>
           </li>
           <li>
             <Link to={"/creator"}>
-              <button className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-blue-700 xl:p-0 focus:text-blue-700">
+              <button className="block py-2 pl-3 pr-4 ml-4 lg:ml-0 xl:ml-0 text-gray-900 rounded hover:text-blue-700 xl:p-0 focus:text-blue-700">
                 ผู้จัดทำ
               </button>
             </Link>
           </li>
           <li>
             <Link to={"/help"}>
-              <button className="block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent hover:text-blue-700 xl:p-0 focus:text-blue-700">
+              <button className="block py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent hover:text-blue-700 ml-4 lg:ml-0 xl:ml-0 xl:p-0 focus:text-blue-700">
                 ติดต่อ/แจ้งปัญหา
               </button>
             </Link>
@@ -45,7 +45,7 @@ const ResponsiveNav = ({ currentUser }) => {
               <Link to={"/login"}>
                 <button
                   type="button"
-                  className="text-white bg-blue-700 block md:block lg:block xl:block 2xl:block hover:bg-blue-800 font-medium rounded-lg text-sm px-4 ml-2 py-2 text-center mb-4 lg:mr-0 dark:bg-blue-600"
+                  className="text-white bg-blue-700 block md:block lg:block xl:block 2xl:block hover:bg-blue-800 font-medium rounded-lg text-sm px-4 ml-4 lg:ml-0 xl:ml-0 py-2 text-center mb-4 lg:mr-0 dark:bg-blue-600"
                 >
                   Login
                 </button>
