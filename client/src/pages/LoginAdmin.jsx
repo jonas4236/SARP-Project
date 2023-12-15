77bc4f31debce52ffcc47d74e09a9070baff67f8
Thanakorn Sangmee <123473752+JnasDev@users.noreply.github.com>
2023-12-15 19:28:43 +0700

    added teacher login page

---
+++ client/src/pages/LoginAdmin.jsx
@@ -0,0 +1,109 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+
+const LoginAdmin = () => {
+  axios.defaults.withCredentials = true;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const { loginAdmin, currentAdmin } = useContext(AuthContext);
+
+  const handleSummit = async (e) => {
+    e.preventDefault();
+    await loginAdmin({ email, password }).then((result) => {
+      if (result.status === "success") {
+        navigate("/").then(() => {
+          location.reload(true);
+        });
+      } else {
+        navigate("/teacher");
+      }
+    });
+  };
+
+  // useEffect(() => {
+  //   axios.get(`${DefaultAPI}/login`).then((res) => {
+  //     if (res.data.redirectTo) {
+  //       window.location.href = res.data.redirectTo;
+  //     }
+  //   });
+  // }, []);
+
+  return (
+    <>
+      <div className="px-4">
+        {currentAdmin ? (
+          ""
+        ) : (
+          <>
+            <div className="h-[100vh] lg:w-[1170px] xl:w-[1170px] rounded-lg mx-auto">
+              <div className="flex gap-4">
+                <div className="flex-[1] flex justify-center">
+                  <div className="h-[100vh] flex items-center">
+                    <div className="p-8 h-max bg-opacity-75 z-50 rounded-lg lg:w-[439px] xl:w-[439px]">
+                      <div className="w-full">
+                        <div className="mt-4">
+                          <div className="">
+                            <div className="">
+                              <img
+                                src="https://res.cloudinary.com/jonasdev/image/upload/v1702551478/sarp-logo-figma_qh583e.png"
+                                alt="SARP_LOGO"
+                              />
+                            </div>
+                            <form onSubmit={handleSummit}>
+                              <div className="w-full mt-4">
+                                <input
+                                  className="w-full rounded-[10px_10px_0_0]"
+                                  type="email"
+                                  name=""
+                                  id=""
+                                  placeholder="Username & Email"
+                                  onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <input
+                                  className="w-full rounded-[0_0_10px_10px]"
+                                  type="password"
+                                  name=""
+                                  id=""
+                                  placeholder="Password"
+                                  onChange={(e) => setPassword(e.target.value)}
+                                />
+                              </div>
+                              <div className="flex flex-col mt-4">
+                                <button className="py-2 bg-gradient-to-r from-sky-500 to-emerald-300 rounded-lg text-white font-medium">
+                                  เข้าสู่ระบบ
+                                </button>
+                                <Link to={"/help"}>
+                                  <div className="w-full flex justify-between mt-2">
+                                    <button className="text-[#2F80ED] font-medium">
+                                      ลืมรหัสผ่าน
+                                    </button>
+                                    <Link to={"/login"}>
+                                      <button className="text-[#2F80ED] font-medium">
+                                        ผู้ปกครอง
+                                      </button>
+                                    </Link>
+                                  </div>
+                                </Link>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default LoginAdmin;
--- client/src/pages/help.jsx
+++ client/src/pages/help.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const help = () => {
   return (
@@ -26,7 +27,7 @@ const help = () => {
           </div>
           <div className="mt-8 flex flex-col items-center justify-center">
             <textarea
-              className="w-[650px] outline-none border-[#BDBDBD] border-[3px] rounded-md resize-none"
+              className="w-[650px] h-[150px] outline-none border-[#BDBDBD] border-[2px] rounded-md resize-none"
               placeholder="แจ้งปัญหาของคุณ..."
               name=""
               id=""
@@ -36,6 +37,11 @@ const help = () => {
             <button className="bg-[#2F80ED] text-white mt-4 py-2 px-16 rounded-lg">
               แจ้งปัญหา
             </button>
+            <Link to={"/"}>
+              <div className="mt-4">
+                <button className="text-[#BDBDBD]">กลับสู่หน้าหลัก</button>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
